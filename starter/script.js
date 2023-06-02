db2b0d279d1bd7fa7a7f6a4c7b71153a2acdce11
Bao <baohuysieuquay@gmail.com>
2023-06-02 11:30:58 +0200

    First Commit

---
+++ starter/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
